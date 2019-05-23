533187375ddbf8e5d52a4dae8c188a66225df4ec
parthpatel041 <Parthkumar.Patel@dal.ca>
2019-05-23 07:30:13 -0700

    comparing all 3 books with 20k and geting output in  text files saprate for each

--- task3.py
+++ task3.py
@@ -1,10 +1,31 @@
+# help from https://stackoverflow.com/questions/19007383/compare-two-different-files-line-by-line-in-python
+
 with open('Book1.txt', 'r') as file1:
     with open('20k.txt', 'r') as file2:
         same = set(file1).intersection(file2)
 
 same.discard('\n')
 
-with open('some_output_file.txt', 'w') as file_out:
+with open('notin20kforBook1.txt', 'w') as file_out:
     for line in same:
         file_out.write(line)
 
+with open('Book2.txt', 'r') as file3:
+    with open('20k.txt', 'r') as file2:
+        same = set(file3).intersection(file2)
+
+same.discard('\n')
+
+with open('notin20kforBook2.txt', 'w') as file_out:
+    for line in same:
+        file_out.write(line)
+
+with open('Book3.txt', 'r') as file4:
+    with open('20k.txt', 'r') as file2:
+        same = set(file4).intersection(file2)
+
+same.discard('\n')
+
+with open('notin20kforBook3.txt', 'w') as file_out:
+    for line in same:
+        file_out.write(line)
