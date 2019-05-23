983a1194685f9c60836bf95b5999c7e83460b9e3
parthpatel041 <Parthkumar.Patel@dal.ca>
2019-05-23 07:00:07 -0700

    printing all the directory and file name of provided path

--- task2.py
+++ task2.py
@@ -1,24 +1,37 @@
 # help from https://thispointer.com/python-how-to-get-list-of-files-in-directory-and-sub-directories/
 
 import os
-os.listdir(path='.')
-
-def getListOfFiles(dirName):
-
+ 
+'''
+    For the path given, proving List of all files in the directory list 
+'''
+def getListOfFiles(dirName):    # create a list of file and sub directories 
     listOfFile = os.listdir(dirName)
     allFiles = list()
 
     for entry in listOfFile:
-				        # Create full path
-        fullPath = os.path.join(dirName, entry)
-       				 # If entry is a directory then get the list of files in this directory 
-        if os.path.isdir(fullPath):
+        fullPath = os.path.join(dirName, entry) 
+        if os.path.isdir(fullPath):           # If entry is a directory then get the list of files in this directory
             allFiles = allFiles + getListOfFiles(fullPath)
         else:
             allFiles.append(fullPath)
-                
     return allFiles
 
-dirName = '/home/student/Downloads';
-				# Get the list of all files in directory tree at given path
-listOfFiles = getListOfFiles(dirName)
+def main():
+    dirName = '/home/student/Downloads';                   # Get the list of all files in directory tree at given path
+    listOfFiles = getListOfFiles(dirName)
+    for elem in listOfFiles:
+       print(elem)
+    print ("****************")
+
+    listOfFiles = list()
+    for (dirpath, dirnames, filenames) in os.walk(dirName):
+        listOfFiles += [os.path.join(dirpath, file) for file in filenames]
+
+    for elem in listOfFiles:                        # Print the files
+        print(elem)    
+
+if __name__ == '__main__':
+    main()
+
+
