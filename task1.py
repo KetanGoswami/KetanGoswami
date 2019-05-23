7174764b6063b25b43f3de89626b0a6b6cb3d4fa
parthpatel041 <Parthkumar.Patel@dal.ca>
2019-05-23 06:31:12 -0700

    taking input from user for task and doing it as per requirement

--- task1.py
+++ task1.py
@@ -1,19 +1,24 @@
-#mylines = []
-#with open ('Book1.txt', 'rt') as newfile: # Open Book1.txt for reading text.
-#    for line in newfile:
-#        mylines.append(line)              # add that line to the list.
-#    for content in mylines:
-#        print(content)                    # print it.
+choice = input("enter choice(1/2/0):")
+#input1= int(input("enter 1 to copy file in to new file: else 0:"))
+#inupt2= int (input("enter 2 if you want to converted text to LEET code: else 0:"))
 
-with open("Book1.txt") as f:
+
+if  choice =='1':
+ with open("Book1.txt") as f:
     with open("out.txt", "w") as f1:   # open a old file read it and  created new one with name out.txt
         for line in f:
             f1.write(line)
-
-def leet(text):
+elif choice =='2':
+ 
+ def leet(text):
     getchar = lambda c: chars[c] if c in chars else c
     chars = {"a":"4","e":"3","l":"1","o":"0","s":"5","A":"4","E":"3","L":"1","O":"0","S":"5"}
     return ''.join(getchar(c) for c in text)
+ print(leet("Converted leet text!"))
+#print(leet("CONVERTED  LEET TEXT!"))
 
-print(leet("Converted leet text!"))
-print(leet("CONVERTED  LEET TEXT!"))
+elif choice =='0':
+   print(" Thank you for your responce")
+else :
+   print("enter valid number for task")
+
