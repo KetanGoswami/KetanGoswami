c7e55f09aac351af7dffe831e9d1b2f5372c13cd
parthpatel041 <Parthkumar.Patel@dal.ca>
2019-05-23 07:23:23 -0700

    opening Book1 and comparing with 20k and trying to write output in one txt file

---
+++ task3.py
@@ -0,0 +1,10 @@
+with open('Book1.txt', 'r') as file1:
+    with open('20k.txt', 'r') as file2:
+        same = set(file1).intersection(file2)
+
+same.discard('\n')
+
+with open('some_output_file.txt', 'w') as file_out:
+    for line in same:
+        file_out.write(line)
+
